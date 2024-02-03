13b613e4dcd7103e5cff69f08af42792292681f9
gwanat <gwanat876@gmail.com>
2024-02-03 20:31:34 +0100

    Right-click event on TurboHoe added

--- src/main/java/net/GGM/projectmod/item/ModItems.java
+++ src/main/java/net/GGM/projectmod/item/ModItems.java
@@ -2,9 +2,8 @@ package net.GGM.projectmod.item;
 
 import net.GGM.projectmod.ProjectMod;
 import net.GGM.projectmod.item.custom.FireSwordItem;
-import net.minecraft.world.item.HoeItem;
+import net.GGM.projectmod.item.custom.KosaItem;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.Tier;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -21,7 +20,7 @@ public class ModItems {
             () -> new Item(new Item.Properties()));
 
     public static final RegistryObject<Item> KOSA = ITEMS.register("kosa",
-            () -> new Item(new Item.Properties()));
+            () -> new KosaItem(new Item.Properties()));
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
     }
