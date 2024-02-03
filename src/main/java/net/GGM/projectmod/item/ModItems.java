c9ef6e041cfa64e51b90c67e221992a1e9297260
gwanat <gwanat876@gmail.com>
2024-02-03 23:04:59 +0100

    Right-click event on TurboHoe added

--- src/main/java/net/GGM/projectmod/item/ModItems.java
+++ src/main/java/net/GGM/projectmod/item/ModItems.java
@@ -2,14 +2,8 @@ package net.GGM.projectmod.item;
 
 import net.GGM.projectmod.ProjectMod;
 import net.GGM.projectmod.item.custom.FireSwordItem;
-
 import net.GGM.projectmod.item.custom.KosaItem;
-
-
-import net.minecraft.world.item.HoeItem;
-
 import net.GGM.projectmod.item.custom.ModFoods;
-
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -26,16 +20,14 @@ public class ModItems {
     public static final RegistryObject<Item> RAW_BRICK = ITEMS.register("raw_brick",
             () -> new Item(new Item.Properties()));
 
-
     public static final RegistryObject<Item> KOSA = ITEMS.register("kosa",
+            () -> new KosaItem(new Item.Properties().durability(80)));
 
-            () -> new KosaItem(new Item.Properties()));
-
+    public static final RegistryObject<Item> GLACIER_INGOT = ITEMS.register("glacier_ingot",
             () -> new Item(new Item.Properties()));
 
     public static final RegistryObject<Item> WSIZSANWICH = ITEMS.register("wsizsandwich",
             () -> new Item(new Item.Properties().food(ModFoods.WSIZSANDWICH)));
-
 
 
     public static void register(IEventBus eventBus){
