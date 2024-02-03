3311c61d7d4cb3e4532293808df9721bde121d47
Gabriela Marek <w67497@student.wsiz.edu.pl>
2024-02-03 20:30:20 +0100

    Recipe for teh sword

--- src/main/java/net/GGM/projectmod/ProjectMod.java
+++ src/main/java/net/GGM/projectmod/ProjectMod.java
@@ -49,8 +49,9 @@ public class ProjectMod
             event.accept(ModItems.RAW_BRICK);
         }
 
-        if(event.getTabKey() == CreativeModeTabs.TOOLS_AND_UTILITIES){
+        if(event.getTabKey() == CreativeModeTabs.TOOLS_AND_UTILITIES) {
             event.accept(ModItems.KOSA);
+        }
 
         if(event.getTabKey() == CreativeModeTabs.FOOD_AND_DRINKS) {
             event.accept(ModItems.WSIZSANWICH);
